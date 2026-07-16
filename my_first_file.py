fd9f3480622d845c4b45a7db7b5c172dadb802e2
Gauri Mahajan <gama2955@colorado.edu>
2026-07-16 11:21:21 -0600

    Learned about types, calculations, and other niceties in Python

---
+++ my_first_file.py
@@ -0,0 +1,8 @@
+print("Hello world")
+print(2+2)
+#Here, nothing gets executed when I press enter
+#how can I run this code? #2 ways
+# 1. you can put the caret on a line and press shift + enter > it is going to send the line to a repl and run it
+print("Hello world")
+#2. "Run" the file > send the entire content of the file in python, and all the lines will be executed in the sequence
+# press the run button at the top right of the central panel > you will want to do this once you've finished writing the script
